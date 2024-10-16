82cc16bdbdf4454cd9c430e7c8125c289d644868
Rauan <rauanconceicao789@gmail.com>
2024-10-16 17:52:15 -0300

    parece o que é o ultimo commit

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -1,32 +1,19 @@
-import { useState, useEffect } from "react"; // Importando useState e useEffect
+import { useState } from "react"; // Importando useState
 import { ComponetHeader } from "./styles";
-import { FaRegHeart, FaRegUserCircle, FaBars } from "react-icons/fa"; // Importando o ícone de menu
+import { FaRegHeart, FaRegUserCircle, FaBars } from "react-icons/fa"; // Importando ícones
 import { useNavigate } from "react-router-dom";
 
 export const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false); 
   const navigate = useNavigate();
-  const [isLoggedIn, setIsLoggedIn] = useState(false); // Estado para controle de 
-
-  useEffect(() => {
-    const token = JSON.parse(localStorage.getItem("doacao")); // Verifica o token no localStorage
-    setIsLoggedIn(!!token); // Atualiza o estado de login
-  }, []);
-
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen); // Alterna o estado do menu
-  };
 
   const handleLogout = () => {
     localStorage.removeItem("doacao"); // Remove o token do localStorage
-    setIsLoggedIn(false); // Atualiza o estado de login
     navigate("/login"); // Redireciona para a página de login
-; // Recarrega a página
   };
 
   const handleLogin = () => {
     navigate("/login"); // Navega para a página de login
-    window.location.reload(); // Recarrega a página ao navegar para login
   };
 
   const handleMinhasDoacoes = () => {
@@ -35,6 +22,16 @@ export const Header = () => {
 
   const handleMinhasCampanhas = () => {
     navigate("/minhascampanhas"); // Navega para a página de Minhas Campanhas
+  };
+
+  // Função para verificar se o usuário está logado
+  const isLoggedIn = () => {
+    const token = localStorage.getItem("doacao");
+    return token !== null;
+  };
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen); // Alterna o estado do menu
   };
 
   return (
@@ -46,10 +43,12 @@ export const Header = () => {
       <nav className={isMenuOpen ? "open" : ""}> {/* Classe para o menu aberto */}
         <p onClick={handleMinhasDoacoes}>Minhas Doações</p>
         <p onClick={handleMinhasCampanhas}>Minhas Campanhas</p>
-        {isLoggedIn ? (
-          <h2 onClick={handleLogout}>
-            <FaRegUserCircle /> Sair
-          </h2>
+        {isLoggedIn() ? (
+          <>
+            <h2 onClick={handleLogout}>
+              <FaRegUserCircle /> Sair
+            </h2>
+          </>
         ) : (
           <h2 onClick={handleLogin}>
             <FaRegUserCircle /> Login
