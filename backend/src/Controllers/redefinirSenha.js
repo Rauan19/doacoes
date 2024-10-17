d45299813cd8f9908fe058aa10b399f8d9475d39
Rauan <rauanconceicao789@gmail.com>
2024-10-17 07:59:32 -0300

    Auste url

--- backend/src/Controllers/redefinirSenha.js
+++ backend/src/Controllers/redefinirSenha.js
@@ -32,7 +32,7 @@ export const enviarEmailRedefinicao = async (req, res) => {
     });
 
     // Criar o link de redefinição de senha
-    const link = `https://doacoes-ashen.vercel.app/redefinir-senha/${token}`;
+    const link = `https://doacoes-2024.vercel.app/redefinir-senha/${token}`;
 
     // Configurar o conteúdo do e-mail
     const mailOptions = {
